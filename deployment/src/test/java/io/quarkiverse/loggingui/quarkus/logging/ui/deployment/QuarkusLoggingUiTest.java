807d918f7ae6e3fa1a4265ed6458c01873fc6a9d
Oscar Albrecht <3311764+oscarfh@users.noreply.github.com>
2020-10-21 22:42:31 -0300

    test case

--- deployment/src/test/java/io/quarkiverse/loggingui/quarkus/logging/ui/deployment/QuarkusLoggingUiTest.java
+++ deployment/src/test/java/io/quarkiverse/loggingui/quarkus/logging/ui/deployment/QuarkusLoggingUiTest.java
@@ -3,7 +3,6 @@ package io.quarkiverse.loggingui.quarkus.logging.ui.deployment;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
 
@@ -24,50 +23,21 @@ class QuarkusLoggingUiTest {
     static final QuarkusUnitTest config = new QuarkusUnitTest();
 
     @Test
-    public void getEndpointListsLoggersAndLevels() {
-        Map<String, Object> response = performGetRequest("");
-        ArrayList<String> levels = (ArrayList) response.get("levels");
-        assertThat(levels.size(), equalTo(9));
-        Map<String, Map<String, String>> descriptionByLoggerName = (Map<String, Map<String, String>>) response.get(
-                "loggers");
-        assertThat(descriptionByLoggerName.size(), greaterThan(1));
-    }
-
-    @Test
-    public void getEndpointListSpecificLogger() {
-        String loggerName = "io.quarkus";
-        Map<String, Object> response = performGetRequest(loggerName);
-        checkGetResponse(response, loggerName);
-    }
-
-    @Test
-    public void getEndpointListROOTLogger() {
-        String loggerName = "ROOT";
-        Map<String, Object> response = performGetRequest(loggerName);
-        checkGetResponse(response, loggerName);
-    }
-
-    @Test
-    public void postEndpointSetsLogLevel() {
-        String loggerName = "ROOT";
-        String newLogLevel = "TRACE";
-        Map<String, Object> response = performGetRequest(loggerName);
-        checkGetResponse(response, loggerName);
-        performPostRequest(loggerName, newLogLevel, 204);
-        Map<String, Object> secondCallResponse = performGetRequest(loggerName);
-        checkGetResponse(secondCallResponse, loggerName, newLogLevel, newLogLevel);
-    }
-
-    @Test
-    public void postEndpointSetsLogLevelAlsoInLowerCase() {
+    public void testChangeState1() {
         String loggerName = "io.quarkus.loggers";
         String newLogLevel = "trace";
         Map<String, Object> response = performGetRequest(loggerName);
         checkGetResponse(response, loggerName);
         performPostRequest(loggerName, newLogLevel, 204);
-        Map<String, Object> secondCallResponse = performGetRequest(loggerName);
-        checkGetResponse(secondCallResponse, loggerName, newLogLevel.toUpperCase(Locale.ROOT),
-                newLogLevel.toUpperCase(Locale.ROOT));
+    }
+
+    @Test
+    public void testChangeState2() {
+        String loggerName = "io.quarkus.loggers";
+        String newLogLevel = "trace";
+        Map<String, Object> response = performGetRequest(loggerName);
+        checkGetResponse(response, loggerName);
+        performPostRequest(loggerName, newLogLevel, 204);
     }
 
     private Map<String, Object> performGetRequest(String loggerName) {
@@ -96,13 +66,6 @@ class QuarkusLoggingUiTest {
         assertThat(logLevelDescription.get("effectiveLevel"), equalTo(effectiveLogLevel));
     }
 
-    @Test
-    public void postEndpointFailsOnUnknownLogLevel() {
-        String loggerName = "ROOT";
-        String newLogLevel = "non-existing";
-        performPostRequest(loggerName, newLogLevel, 400);
-    }
-
     private void performPostRequest(String loggerName, String newLogLevel, int expectedStatusCode) {
         given().contentType("application/json")
                 .body(String.format("{\"configuredLevel\": \"%s\"}", newLogLevel))
@@ -110,10 +73,4 @@ class QuarkusLoggingUiTest {
                 .post("/loggers/{loggerName}", loggerName)
                 .then().statusCode(expectedStatusCode);
     }
-
-    // TODO:
-    // restart quarkus on every new test?
-    // force authentication
-    // Other technologied (slf4j?) compatible?
-
 }
