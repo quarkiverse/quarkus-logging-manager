c61c42934ed2db2afefcb44621a638dc1e9c48d1
Stefan Gürtler <stefan.guertler@swarco.com>
2023-10-24 10:24:04 +0200

    Mention OpenAPI in method name

--- deployment/src/main/java/io/quarkiverse/loggingmanager/deployment/LoggingManagerProcessor.java
+++ deployment/src/main/java/io/quarkiverse/loggingmanager/deployment/LoggingManagerProcessor.java
@@ -84,7 +84,7 @@ class LoggingManagerProcessor {
             ManagementInterfaceBuildTimeConfig managementConfig) {
 
         // Add to OpenAPI if OpenAPI is available
-        if (capabilities.isPresent(Capability.SMALLRYE_OPENAPI) && shouldInclude(launchMode, loggingManagerConfig, managementConfig)) {
+        if (capabilities.isPresent(Capability.SMALLRYE_OPENAPI) && shouldIncludeInOpenAPI(launchMode, loggingManagerConfig, managementConfig)) {
             LoggingManagerOpenAPIFilter filter = new LoggingManagerOpenAPIFilter(
                     nonApplicationRootPathBuildItem.resolvePath(loggingManagerConfig.basePath),
                     loggingManagerConfig.openapiTag);
@@ -96,7 +96,7 @@ class LoggingManagerProcessor {
         return launchMode.getLaunchMode().isDevOrTest() || loggingManagerConfig.alwaysInclude;
     }
 
-    private static boolean shouldInclude(LaunchModeBuildItem launchMode, LoggingManagerConfig loggingManagerConfig, ManagementInterfaceBuildTimeConfig managementConfig) {
+    private static boolean shouldIncludeInOpenAPI(LaunchModeBuildItem launchMode, LoggingManagerConfig loggingManagerConfig, ManagementInterfaceBuildTimeConfig managementConfig) {
         return !managementConfig.enabled && shouldInclude(launchMode, loggingManagerConfig);
     }
 
