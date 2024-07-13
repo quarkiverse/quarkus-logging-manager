854fd75305406d481c23d578189f702f542a0e8a
Melloware <mellowaredev@gmail.com>
2024-07-13 08:33:58 -0400

    Change invalid YoutTube icon to Play Circle

--- deployment/src/main/java/io/quarkiverse/loggingmanager/deployment/devui/LoggingManagerDevUIProcessor.java
+++ deployment/src/main/java/io/quarkiverse/loggingmanager/deployment/devui/LoggingManagerDevUIProcessor.java
@@ -28,7 +28,7 @@ public class LoggingManagerDevUIProcessor {
                 .icon("font-awesome-solid:layer-group"));
 
         pageBuildItem.addPage(Page.webComponentPageBuilder()
-                .icon("font-awesome-solid:youtube")
+                .icon("font-awesome-solid:play-circle")
                 .componentLink("qwc-logging-manager-loggers.js"));
 
         return pageBuildItem;
