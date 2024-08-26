52f9e908ba6d252c079a14d8145ec39acc7476ba
Melloware <mellowaredev@gmail.com>
2024-08-26 07:40:38 -0400

    Update to @ConfigMapping (#323)

--- runtime/src/main/java/io/quarkiverse/loggingmanager/LoggingManagerRuntimeConfig.java
+++ runtime/src/main/java/io/quarkiverse/loggingmanager/LoggingManagerRuntimeConfig.java
@@ -1,16 +1,18 @@
 package io.quarkiverse.loggingmanager;
 
-import io.quarkus.runtime.annotations.ConfigItem;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithDefault;
 
-@ConfigRoot(name = "logging-manager", phase = ConfigPhase.RUN_TIME)
-public class LoggingManagerRuntimeConfig {
+@ConfigMapping(prefix = "quarkus.logging-manager")
+@ConfigRoot(phase = ConfigPhase.RUN_TIME)
+public interface LoggingManagerRuntimeConfig {
 
     /**
      * If Logging Manager should be enabled. By default, Logging Manager is enabled if it is included (see
      * {@code always-include}).
      */
-    @ConfigItem(name = "enable", defaultValue = "true")
-    boolean enable;
+    @WithDefault("true")
+    boolean enable();
 }
